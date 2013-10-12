230a85fe473d4216fd7ec8429e86c03f9dd069e4
xunzhang <xunzhangthu@gmail.com>
2013-10-12 17:26:31 +0800

    gen_md5digest.py

--- gen_md5digest.py
+++ gen_md5digest.py
@@ -1,5 +1,10 @@
+#! /usr/bin/python
+# used for doubanm hpp files
+
 if __name__ == '__main__':
     import sys
     import hashlib
     m = hashlib.md5(sys.argv[1])
-    print m.hexdigest()
+    res = m.hexdigest()
+    lst = ['FILE_', res[:8], res[8:12], res[12:16], res[16:20], res[20:], '_H']
+    print '_'.join(lst)
